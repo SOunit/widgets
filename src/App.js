cee8a3fd89e44fbd880fee3d375337f0ca855b1f
SOUnit <workbookwork7@gmail.com>
2021-03-14 19:28:28 +0900

    added semantic style

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
 import React from 'react';
+import Accordion from './components/Accordion';
+
+const items = [
+  {
+    title: 'Wtat is React?',
+    content: 'React is a front end javascript framework',
+  },
+  {
+    title: 'Wtat is React?',
+    content: 'React is a front end javascript framework',
+  },
+  {
+    title: 'Wtat is React?',
+    content: 'React is a front end javascript framework',
+  },
+];
 
 export default () => {
-  return <h1>Widgets App</h1>;
+  return (
+    <div>
+      <Accordion items={items} />
+    </div>
+  );
 };
