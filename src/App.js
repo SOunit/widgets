583e02b1886024eae81112e52c6ef275d6fa3e24
SOUnit <workbookwork7@gmail.com>
2021-03-16 13:11:45 +0900

    added connection of item and selected

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Dropdown from './components/Dropdown';
 
 const items = [
@@ -32,9 +32,15 @@ const options = [
 ];
 
 export default () => {
+  const [selected, setSelected] = useState(options[0]);
+
   return (
     <div>
-      <Dropdown options={options} />
+      <Dropdown
+        selected={selected}
+        onSelectedChange={setSelected}
+        options={options}
+      />
     </div>
   );
 };
