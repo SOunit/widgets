a6c27dce21bcd8becabb0be7ce9fe59f34cf53fd
SOUnit <workbookwork7@gmail.com>
2021-03-19 08:47:00 +0900

    added route component

--- src/components/memo/05_App_show_item_by_path.js
+++ src/components/memo/05_App_show_item_by_path.js
@@ -3,7 +3,6 @@ import Accordion from './components/Accordion';
 import Search from './components/Search';
 import Dropdown from './components/Dropdown';
 import Translate from './components/Translate';
-import Route from './components/Route';
 
 const items = [
   {
@@ -35,28 +34,46 @@ const options = [
   },
 ];
 
+const showAccordion = () => {
+  if (window.location.pathname === '/') {
+    return <Accordion items={items} />;
+  }
+};
+
+const showList = () => {
+  if (window.location.pathname === '/list') {
+    return <Search />;
+  }
+};
+
+const showDropdown = (selected, setSelected) => {
+  if (window.location.pathname === '/dropdown') {
+    return (
+      <Dropdown
+        label='test'
+        options={options}
+        selected={selected}
+        onSelectedChange={setSelected}
+      />
+    );
+  }
+};
+
+const showTranslate = () => {
+  if (window.location.pathname === '/translate') {
+    return <Translate />;
+  }
+};
+
 export default () => {
   const [selected, setSelected] = useState(options[0]);
 
   return (
     <div>
-      <Route path='/'>
-        <Accordion items={items} />
-      </Route>
-      <Route path='/list'>
-        <Search />
-      </Route>
-      <Route path='/dropdown'>
-        <Dropdown
-          label='Select a Color'
-          options={options}
-          selected={selected}
-          onSelectedChange={setSelected}
-        />
-      </Route>
-      <Route path='/translate'>
-        <Translate options={options} />
-      </Route>
+      {showAccordion()}
+      {showList()}
+      {showDropdown(selected, setSelected)}
+      {showTranslate()}
     </div>
   );
 };
