4737d5cfc55a769f0970d3608298cd39716eafbd
SOUnit <workbookwork7@gmail.com>
2021-03-18 13:23:05 +0900

    added timer to decrease sending http request

--- src/components/Convert.js
+++ src/components/Convert.js
@@ -2,21 +2,45 @@ import axios from 'axios';
 import React, { useState, useEffect } from 'react';
 
 const Convert = ({ language, text }) => {
-  useEffect(() => {
-    axios.post(
-      'https://translation.googleapis.com/language/translate/v2',
-      {},
-      {
-        params: {
-          q: text,
-          target: language.value,
-          key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM',
-        },
-      }
-    );
-  }, [language, text]);
+  // this is updated every time user input
+  const [translated, setTranslated] = useState('');
+  // this is updated only if use input stops for 500ms
+  const [debouncedText, setDebouncedText] = useState(text);
 
-  return <div></div>;
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedText(text);
+    }, 500);
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [text]);
+
+  useEffect(() => {
+    const doTranslation = async () => {
+      const { data } = await axios.post(
+        'https://translation.googleapis.com/language/translate/v2',
+        {},
+        {
+          params: {
+            q: debouncedText,
+            target: language.value,
+            key: 'AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM',
+          },
+        }
+      );
+      setTranslated(data.data.translations[0].translatedText);
+    };
+
+    doTranslation();
+  }, [language, debouncedText]);
+
+  return (
+    <div>
+      <h1 className='ui header'>{translated}</h1>
+    </div>
+  );
 };
 
 export default Convert;
