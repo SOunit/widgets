971614c23da3316d7973868d1fd7a4dce2111718
SOUnit <workbookwork7@gmail.com>
2021-03-20 10:54:50 +0900

    added command key check

--- src/components/Link.js
+++ src/components/Link.js
@@ -2,6 +2,11 @@ import React from 'react';
 
 const Link = ({ className, href, children }) => {
   const onClick = (event) => {
+    // for command + click, which opens new tab, default action is ok
+    if (event.metaKey || event.ctrlKey) {
+      return;
+    }
+
     event.preventDefault();
     window.history.pushState({}, '', href);
 
